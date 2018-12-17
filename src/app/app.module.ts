b7ccaca4af657a261aa857edc2e756a903dbffb0
JuanjoCobo <jjcobojimenez@gmail.com>
2018-12-17 15:09:24 +0000

    funcionalidad de la app finalizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { FormsModule} from '@angular/forms';
 import { AutofocusModule } from 'angular-autofocus-fix';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { TasksComponent } from './components/tasks/tasks.component';
@@ -15,7 +16,8 @@ import { TasksComponent } from './components/tasks/tasks.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AutofocusModule
+    AutofocusModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,42 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Task } from '../../interfaces/Task';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.scss']
+  styleUrls: ['./tasks.component.scss'],
+  animations: [
+    trigger('fade', [
+
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(30px)' }),
+        animate(200, style({ opacity: 1, transform: 'translateY(0px)'}))
+      ]),
+
+      transition(':leave', [
+        animate(200, style({ opacity: 0, transform:'translateY(30px)' }))
+      ]),
+
+    ])
+  ]
 })
+
 export class TasksComponent implements OnInit {
 
 	tasks: Task[];
 	idTask: number;
 	taskTitle: string;
 	beforeEditCache: string;
+  filter:string;
 
   constructor() { }
 
   ngOnInit() {
 
+    this.filter = 'all';
   	this.beforeEditCache = '';
   	this.idTask = 4;
   	this.taskTitle = '';
   	this.tasks = [
   		{
   			'id': 1,
-  			'title': 'Finish Web App with Angular7',
+  			'title': 'Tarea ejemplo 1',
   			'completed': false,
   			'editing':false,
   		},
   		{
   			'id': 2,
-  			'title': 'Send email to Juan',
+  			'title': 'Tarea ejemplo 2',
   			'completed': false,
   			'editing':false,
   		},
   		{
   			'id': 3,
-  			'title': 'Shut up my mouth',
+  			'title': 'Tarea ejemplo 3',
   			'completed': false,
   			'editing':false,
   		},
@@ -84,6 +102,36 @@ export class TasksComponent implements OnInit {
 
   deleteTask(id:number): void{
   	this.tasks = this.tasks.filter(task => task.id !== id);
+  }
+
+  //muestra el numero de tareas que no están completas/pendientes
+  remaining():number{
+    return this.tasks.filter(task => !task.completed).length;
+  }
+
+  //permite mostrar boton limpiar completado siempre que haya al menos una tarea completada
+  atLeastOneCompleted():boolean{
+     return this.tasks.filter(task => task.completed).length > 0;
+  }
+
+  clearCompleted():void{
+    this.tasks = this.tasks.filter(task => !task.completed);
+  }
+
+  checkAllTasks():void{
+    this.tasks.forEach(task => task.completed = (<HTMLInputElement>event.target).checked);
+  }
+
+  tasksFiltered(): Task[] {
+    if(this.filter == 'all'){
+      return this.tasks;
+    }else if(this.filter == 'active'){
+      return this.tasks.filter(task => !task.completed);
+    }else if(this.filter == 'completed'){
+      return this.tasks.filter(task => task.completed);
+    }
+
+    return this.tasks;
   }
 
 }
